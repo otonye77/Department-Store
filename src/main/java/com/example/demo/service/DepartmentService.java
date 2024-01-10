4f4a4882669a1e3fe8330eac4dcb353d826b6be7
otonye77 <oamietubodie@gmail.com>
2024-01-10 12:38:20 +0100

    get api done

--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -2,6 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+    public List<Department> fetchDepartmentList();
 }
--- src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
+++ src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentServiceImplementation implements DepartmentService  {
     @Autowired
@@ -12,5 +14,9 @@ public class DepartmentServiceImplementation implements DepartmentService  {
     @Override
     public Department saveDepartment(Department department) {
         return departmentRepository.save(department);
+    }
+    @Override
+    public List<Department> fetchDepartmentList(){
+        return departmentRepository.findAll();
     }
 }
