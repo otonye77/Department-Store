dec2c4c9e0573c93a65417a04f248966df2c6333
otonye77 <oamietubodie@gmail.com>
2024-01-11 12:00:36 +0100

    features

--- src/main/java/com/example/demo/Controller/DepartmentController.java
+++ src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -32,4 +32,9 @@ public class DepartmentController {
         return "Department was deleted successfully";
     }
 
+    @PutMapping("/department/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department){
+        return departmentService.updateDepartment(departmentId, department);
+    }
+
 }
